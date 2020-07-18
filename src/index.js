d87ad63642e8559adec357c837fb3f9a8ddba7b9
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-18 14:45:07 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+  let submit = document.getElementById('create-task-form');
+  submit.addEventListener('submit', Submit);
+  
   function Submit (e){
     preventDefault();
     let input = document.getElementById("new-task-description").value = "Johnny Bravo";
-    let list = document.querySelector("div.list ul");
-    list.innerHTML= `<li> ${input} </li>`;
+    //let list = document.querySelector("div.list ul");
+    //list.innerHTML= `<li> ${input} </li>`;
+    console.log(input);
 
   }
 
-  let submit = document.getElementById('create-task-form');
-  submit.addEventListener('submit', Submit);
+  
 });
