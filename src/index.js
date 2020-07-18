455d6e552118cebe4f0c04b39ee750a204b661cf
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-18 14:55:00 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let submit = document.getElementById('create-task-form');
   submit.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(input);
+    console.log("input");
 
 
   });
