2a8643278ddbe30127f002426d87031c9d1c330e
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-18 14:37:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
   function Submit (e){
+    preventDefault();
     let input = document.getElementById("new-task-description").value = "Johnny Bravo";
     let list = document.querySelector("div.list ul");
     list.innerHTML= `<li> ${input} </li>`;
+    
   }
 
   let submit = document.getElementById('id');
