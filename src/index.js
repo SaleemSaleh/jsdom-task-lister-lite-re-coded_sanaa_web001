4301594330aee5929e605377c839a23eccbbc488
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-18 15:19:12 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let submit = document.getElementById('create-task-form');
+  submit.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let input = document.getElementById("new-task-description").value;
+     let list = document.querySelector("#tasks");
+     let li = document.createElement('li');
+     li.innerHTML =`${input} `;
+     list.appendChild(li);
+     console.log(list);
+
+
+  });
+  /*
+  function Submit (e){
+
+    let input = document.getElementById("new-task-description").value;
+    let list = document.querySelector("div.list ul");
+    list.innerHTML= `<li> ${input} </li>`;
+    console.log(input);
+    */
+
+
+
+
 });
